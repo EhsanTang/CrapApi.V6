983768584e18ed823a5856ef001388bc9f06e79e
IhsanTang <IhsanTang@163.com>
2016-10-22 18:12:31 +0800

    fix bug

--- api/src/main/java/cn/crap/controller/front/FrontInterfaceController.java
+++ api/src/main/java/cn/crap/controller/front/FrontInterfaceController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
